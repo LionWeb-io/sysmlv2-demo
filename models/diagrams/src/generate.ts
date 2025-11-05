d8185432ae09e608042ae27a29d50f0cd223ed48
Meinte Boersma <meinte.boersma@gmail.com>
2025-11-05 16:38:08 +0200

    (also generate artifacts for KerML)

--- models/diagrams/src/generate.ts
+++ models/diagrams/src/generate.ts
@@ -9,10 +9,16 @@ import { focusedDiagram } from "./custom-diagram"
 
 const typesLanguage = deserializeLanguagesWithHandler(readFileAsJson("../types_lionweb.json") as LionWebJsonChunk, defaultSimplisticHandler)[0]
 const sysMLv2Language = deserializeLanguagesWithHandler(readFileAsJson("../SysML_lionweb_lionweb.json") as LionWebJsonChunk, defaultSimplisticHandler, typesLanguage)[0]
+const kerMLlanguage = deserializeLanguagesWithHandler(readFileAsJson("../kerml_lionweb_lionweb.json") as LionWebJsonChunk, defaultSimplisticHandler, typesLanguage)[0]
 
 setEOLStyleFromOS()
 
 writeFileSync("artifacts/types.puml", generatePlantUmlForLanguage(typesLanguage))
+writeFileSync("artifacts/types.txt", languageAsText(typesLanguage))
+
+writeFileSync("artifacts/kerml.puml", generatePlantUmlForLanguage(kerMLlanguage))
+writeFileSync("artifacts/kerml.txt", languageAsText(kerMLlanguage))
+
 writeFileSync("artifacts/sysml.puml", generatePlantUmlForLanguage(sysMLv2Language))
 writeFileSync("artifacts/sysml.txt", languageAsText(sysMLv2Language))
 
